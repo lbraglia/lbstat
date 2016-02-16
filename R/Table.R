13cdfc5f058284ceb6237d208bae7a7068c3a94f
Luca Braglia <lbraglia@gmail.com>
2016-02-16 12:17:20 +0100

    Table a bit more generalized

--- R/Table.R
+++ R/Table.R
@@ -4,7 +4,13 @@
 #' default) and addsmargins. 
 #' 
 #' @param ... Arguments to be passed to table.
+#' @param useNA display NA counts
+#' @param f function to be used for summaries
 #' @return The function return same results of table with NA (if present) and
 #' margins.
+#' @examples
+#' with(airquality, Table(Month, Day))
 #' @export
-Table <- function(...) addmargins(base::table(useNA = 'ifany', ...))
+Table <- function(..., useNA = 'ifany', f = list('Sum' = sum))
+    addmargins(base::table(useNA = useNA, ...),
+               FUN = f)
