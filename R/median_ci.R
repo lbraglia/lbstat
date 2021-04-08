027bb3dc8f9e9e4df3e1507a7becb403902bdb2a
Luca Braglia <lbraglia@gmail.com>
2021-04-08 17:19:33 +0200

    update

--- R/median_ci.R
+++ R/median_ci.R
@@ -1,36 +1,65 @@
-#' Quick median with bootstrap confidence interval for
-#' numeric and ordered factor
+#' Median bootstrap confidence interval (BCA or parametric for ordered
+#' factors) for numeric and ordered factor
 #' 
 #' @param x data: an integer, numeric or ordered factor
-#' @param N bootstrap repetitions
+#' @param conf confidence.level
+#' @param R bootstrap repetitions
 #'
 #'@export 
-median_ci <- function(x, N = 10000){ # x ordered factor
-    if (is.ordered(x)) {
-        type <- 1
-        fun <- function(x) quantile(x, probs = 0.5, type = type, na.rm = TRUE)
-    } else {
-        type <- 7
-        fun <- function(x) stats::median(x, na.rm = TRUE)
-    }
-    res <- NULL
-    ## loop over n
-    for (i in seq_len(N)) {
-        bootsample <- sample(x, replace = TRUE)
-        res[i] <- fun(bootsample)
-    }
-    
-    ## per qualche motivo nell'assegnazione viene downgradato
-    ## ad intero, lo riporto in factor
-    if (is.ordered(x)) {
-        res <- factor(res,
-                      levels = seq_len(nlevels(x)),
-                      labels = levels(x),
-                      ordered = TRUE)
-    }
+median_ci <- function(x, conf = 0.95, R = 10000){
+    ord <- is.ordered(x)
+    f <- if (ord) {
+             function(y) stats::quantile(y, probs = 0.5, type = 1, na.rm = TRUE)
+         } else { 
+             function(y) stats::median(y, na.rm = TRUE)
+         }
+    boot_f <- function(data, i) f(data[i])
+    res <-  boot::boot(data = x, statistic = boot_f, R = R)
+    ci <- boot.ci(res, type = if (ord) 'perc' else 'bca', conf = conf)
+    est <- f(x)
+    boot_ci <- ci[[4]][4:5]
+    res <- c(est, boot_ci)
+    nm <- c('Est', 'Lower', "Upper")
+    if (ord) setNames(levels(x)[res], nm = nm)
+    else setNames(res, nm = nm)
+}
 
-    ci <- as.list(quantile(res, probs = c(0.025, 0.975),
-                           type = type, na.rm = TRUE))
-    res <- data.frame('median' = fun(x), ci)
-    setNames(res, c('median', 'lower boot CI', 'upper boot CI'))
+#' Median bootstrap confidence interval (BCA or parametric for ordered
+#' factors) for numeric and ordered factor
+#' 
+#' @param x data from the first group
+#' @param y data from the second group
+#' @param R bootstrap repetition
+#' @param test add the mood test?
+#'
+#'@export 
+median_diff_ci <- function(x, y, R = 10000, 
+                           test = c('mood', 'none')){
+    if (is.ordered(x) || is.ordered(y)) 
+        stop("Only numeric variables")
+    test <- match.arg(test)
+    ## questa funzione non usa data.frame e la roba perché
+    ## richiede che abbiano la stessa lunghezza x e y
+    ## estimante
+    f <- function(z) stats::median(z, na.rm = TRUE)
+    est <- f(y) - f(x) 
+    ## percentile confidence interval
+    res <- NULL
+    for(i in seq_len(R)){
+        ## crea campioni bootstrap
+        x_boot <- sample(x, replace = TRUE)
+        y_boot <- sample(y, replace = TRUE)
+        res <- c(res, f(y_boot) - f(x_boot))
+    }
+    boot_ci <- quantile(res, probs = c(0.025, 0.975))
+    ## intervallo di confidenza
+    rval <- c(est, boot_ci)
+    nm <- c('Est', 'Lower', "Upper")
+    rval <- setNames(rval, nm = nm)
+    rval <- as.data.frame(as.list(rval))
+    ## test di mood
+    if (test == 'mood'){
+        rval$p_value <- pretty_pval(mood.test(x = x, y = y)$p.value)
+    }
+    rval
 }
---
+++ R/mood.test.R
@@ -0,0 +1,19 @@
+#' Mood median test
+#' 
+#' @param x first group of data
+#' @param y second group of data
+#' @export
+mood.test <- function(x, y){
+    data <- c(x, y)
+    group <- factor(c(rep(1, length(x)), rep(2, length(y))))
+    med <- median(data, na.rm = TRUE)
+    data2 <- factor(data >= med, c(FALSE, TRUE), 
+                    labels = c('< med', '>= med'))
+    tab <- table(data2, group)
+    test <- if (fisher_needed(tab)){
+                fisher.test(tab)
+            } else {
+                chisq.test(tab)
+            }
+    list(tab = tab, p.value = test$p.value)
+}
